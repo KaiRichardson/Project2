c6947054adc63c0cf5168da75656bb45b7f4095d
Kai R <kairichardson2000@gmail.com>
2019-06-14 14:11:31 -0700

    server build out

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-// This file should be named `server.js`
+// server.js
 
-const express = require('express');
+const express = require('express');  
+const passport = require('passport');  
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();  
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8050;
 
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 
+// Set up passport strategy
+passport.use(new GoogleStrategy(  
+  {
+    clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
+    callbackURL: 'https://frozen-spire-30925.herokuapp.com/auth/google/callback',
+    scope: ['email'],
+  },
+  // This is a "verify" function required by all Passport strategies
+  (accessToken, refreshToken, profile, cb) => {
+    console.log('Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:', profile);
+    return cb(null, profile);
+  },
+));
+
 // Serve a test API endpoint
-// This is just to test your API -- we're gonna delete this endpoint later
 app.get('/test', (req, res) => {  
   res.send('Your api is working!');
 });
