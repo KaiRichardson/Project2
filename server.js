eaef281356cce2a49e89907d0f2a6f8fc37bc5eb
Kai R <kairichardson2000@gmail.com>
2019-06-14 14:28:42 -0700

    getting page to load

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 8050;
 
 // Serve static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/ligin.html'));
 
 // Set up passport strategy
 passport.use(new GoogleStrategy(  
