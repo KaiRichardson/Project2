7a4d6926bd6d12c9e9501824a9745da10d7a4f64
Kai R <kairichardson2000@gmail.com>
2019-06-14 15:05:41 -0700

    adding more scope

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ passport.use(new GoogleStrategy(
     clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
     clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
     callbackURL: 'https://frozen-spire-30925.herokuapp.com/auth/google/callback',
-    scope: ['email'],
+    scope: ['email', 'profile'],
   },
   (accessToken, refreshToken, profile, cb) => {
     console.log(`Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user: ${profile}`);
