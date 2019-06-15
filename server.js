af1b147bbf62aaf012dfbfcffe6d54468682eae3
Kai R <kairichardson2000@gmail.com>
2019-06-14 22:34:54 -0700

    adding to db

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 var path = require("path");
 var express = require("express");
-var passport = require("passport");
-// var GoogleStrategy = require("passport-google-oauth20").Strategy;
-var session = require("express-session");
+
+var db = require("./models");
 
 var app = express();
 var port = process.env.PORT || 8050;
@@ -39,7 +38,23 @@ app.get("/dashboard", accessProtectionMiddleware, (req, res) => {
   res.sendFile(path.join(__dirname, "./public/dashboard.html"));
 });
 
-// Start server
-var server = app.listen(port, function() {
-  console.log("Server listening on port " + port);
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(port, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      port,
+      port
+    );
+  });
 });
+
+module.exports = app;
