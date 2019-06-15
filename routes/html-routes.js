b43aade902ac87ad805b9f5c9d7fac2e9a1c9bbd
Kai R <kairichardson2000@gmail.com>
2019-06-14 17:44:38 -0700

    html routs fix

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,18 +1,17 @@
 var path = require("path");
-var server = require("../server");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // new user route loads newUser.html
   app.get("/newuser", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/newUser.html"));
+    res.sendFile(path.join(__dirname, "../public/newUser.html"));
   });
 
-  
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
