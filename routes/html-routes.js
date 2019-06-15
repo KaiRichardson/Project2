bd910563e83ab1e278fe11b53a40703f2db83d54
Kai R <kairichardson2000@gmail.com>
2019-06-14 20:11:06 -0700

    goog fix

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,25 +2,25 @@ var path = require("path");
 // var auth = require("../config/passport-setup.js").accessProtectionMiddleware;
 
 module.exports = function(app) {
-    // Checks if a user is logged in
-    var accessProtectionMiddleware = (req, res, next) => {
-      if (req.isAuthenticated()) {
-        next();
-      } else {
-        res.status(403).json({
-          message: "must be logged in to continue"
-        });
-      }
-    };
-  
-  
+  // Checks if a user is logged in
+  var accessProtectionMiddleware = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      res.status(403).json({
+        message: "must be logged in to continue"
+      });
+    }
+  };
+
   // Load index page
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/dashboard", accessProtectionMiddleware, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/dashboard.html"));  });
+    res.sendFile(path.join(__dirname, "../public/dashboard.html"));
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
--- server.js
+++ server.js
@@ -1,78 +1,88 @@
 require("dotenv").config();
-var express = require('express');  
-var passport = require('passport');  
-var GoogleStrategy = require('passport-google-oauth20').Strategy;  
-var session = require('express-session');
+var express = require("express");
+var passport = require("passport");
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
+var session = require("express-session");
 
-var app = express();  
+var app = express();
 var port = process.env.PORT || 8050;
 
 // Serve static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // Add session support
-app.use(session({  
-  secret: process.env.SESSION_SECRET || 'default_session_secret',
-  resave: false,
-  saveUninitialized: false,
-}));
-app.use(passport.initialize());  
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "default_session_secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {  
+passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-passport.deserializeUser((userDataFromCookie, done) => {  
+passport.deserializeUser((userDataFromCookie, done) => {
   done(null, userDataFromCookie);
 });
 
 // Checks if a user is logged in
-var accessProtectionMiddleware = (req, res, next) => {  
+var accessProtectionMiddleware = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
   } else {
     res.status(403).json({
-      message: 'must be logged in to continue',
+      message: "must be logged in to continue"
     });
   }
 };
 
 // Set up passport strategy
-passport.use(new GoogleStrategy(  
-  {
-    clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_OAUTH_TEST_APP_URL,
-    scope: ['email'],
-  },
-  (accessToken, refreshToken, profile, cb) => {
-    console.log('Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:', profile);
-    return cb(null, profile);
-  },
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_OAUTH_TEST_APP_URL,
+      scope: ["email"]
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(
+        "Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:",
+        profile
+      );
+      return cb(null, profile);
+    }
+  )
+);
 
 // Create API endpoints
 
 // This is where users point their browsers in order to get logged in
 // This is also where Google sends back information to our app once a user authenticates with Google
-app.get('/auth/google/callback',  
-  passport.authenticate('google', { failureRedirect: '/', session: true }),
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/", session: true }),
   (req, res) => {
-    console.log('wooo we authenticated, here is our user object:', req.user);
+    console.log("wooo we authenticated, here is our user object:", req.user);
     // res.json(req.user);
-    res.redirect('/');
+    res.redirect("/");
   }
 );
 
-app.get('/protected', accessProtectionMiddleware, (req, res) => {  
-  res.json({
-    message: 'You have accessed the protected endpoint!',
-    yourUserInfo: req.user,
-  });
+// Load index page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/login.html"));
+});
+
+app.get("/dashboard", accessProtectionMiddleware, (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/dashboard.html"));
 });
 
 // Start server
-var server = app.listen(port, function() {  
-  console.log('Server listening on port ' + port);
+var server = app.listen(port, function() {
+  console.log("Server listening on port " + port);
 });
